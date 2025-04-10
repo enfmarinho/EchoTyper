8743a355a7682d97b596ecbfd5334523eca93107
Kael Paraguassu <kaelparaguassu@hotmail.com>
2025-04-10 15:43:24 -0300

    criando páginas de login e cadastro

---
+++ echo-typer/src/app/login/page.tsx
@@ -0,0 +1,71 @@
+'use client'
+import React from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  Link,
+} from "@mui/material";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+    const router = useRouter();
+  
+    return (
+    //   <AuthContainer>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            height: "100vh",
+            backgroundColor: "#122232"
+          }}
+        >
+        <Image src="/logo.png" alt="Logo" width={450} height={450} />
+  
+        <Box
+            sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            maxWidth: 500,
+            gap: 2,
+            }}
+        >
+            <TextField
+            label="Login"
+            variant="filled"
+            fullWidth
+            size="small"
+            sx={{ mb: 2, backgroundColor: "white", borderRadius: 1 }}
+            />
+            <TextField
+            label="Senha"
+            type="password"
+            variant="filled"
+            fullWidth
+            size="small"
+            sx={{ mb: 2, backgroundColor: "white", borderRadius: 1 }}
+            />
+            <Button
+            variant="contained"
+            fullWidth
+            sx={{ mt: 1, backgroundColor: "#C0821A", textTransform: "none" }}
+            onClick={() => {
+                // Aqui pode adicionar a lógica de autenticação
+                router.push("/dashboard/home");
+            } }
+            >
+            Entrar
+            </Button>
+            <Link href="/cadastro" underline="hover" sx={{ color: "#C0821A", textAlign: "center" }}>
+            Criar conta
+            </Link>
+        </Box>
+      </Box>
+     // </AuthContainer>
+    );
+  }
