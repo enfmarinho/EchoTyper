dd9e8a21ee43d9d01183f656a88c0de9cfe5f438
Samuel <samuelmorais552@gmail.com>
2025-04-22 16:41:38 -0300

    chore: Limpeza de dependencias e classe UserConstraints para centralizar a logica de verificacao dos dados de usuario

--- backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/user/dto/UserResponseDTO.java
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/user/dto/UserResponseDTO.java
@@ -1,10 +1,6 @@
 package br.ufrn.EchoTyper.user.dto;
 
-import java.util.UUID;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.validation.constraints.Pattern;
 
 public record UserResponseDTO(
         @JsonProperty("username") String username,
