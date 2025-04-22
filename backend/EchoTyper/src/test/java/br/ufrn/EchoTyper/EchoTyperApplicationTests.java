b6ba31e8c86cd7098acf736d55237f8b539a2990
Samuel <samuelmorais552@gmail.com>
2025-04-22 15:57:53 -0300

    Estrutura base do backend

---
+++ backend/EchoTyper/src/test/java/br/ufrn/EchoTyper/EchoTyperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.EchoTyper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoTyperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
