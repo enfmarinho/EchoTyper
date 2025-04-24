762a2e3edb49899de1f6d3ab0f5f57ee46e63188
Samuel <samuelmorais552@gmail.com>
2025-04-23 22:10:24 -0300

    chore: reestruturacao dos pacotes de User

---
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/user/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package br.ufrn.EchoTyper.user.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.ufrn.EchoTyper.user.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+   public Optional<User> findById(Long id);
+   public Optional<User> findByUsername(String username);
+   public Optional<User> findByEmail(String email);
+
+   @Query("SELECT u.friends FROM User u WHERE u.id = :id")
+   public Optional<List<User>> findAllFriends(@Param("id") Long id);
+   
+   @Query("SELECT u.friends FROM User u WHERE u.username = :username")
+   public Optional<User> findFriendByUsername(@Param("username") String username);
+}
---
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/user/service/UserMapper.java
@@ -0,0 +1,15 @@
+package br.ufrn.EchoTyper.user.service;
+
+import br.ufrn.EchoTyper.user.dto.UserRequestDTO;
+import br.ufrn.EchoTyper.user.dto.UserResponseDTO;
+import br.ufrn.EchoTyper.user.model.User;
+
+public class UserMapper {
+    public static User toEntity(UserRequestDTO userRequestDTO) {
+     return new User(null, userRequestDTO.username(),userRequestDTO.email(), userRequestDTO.password());
+    }
+
+    public static UserResponseDTO toResponseDTO(User user) {
+        return new UserResponseDTO(user.getUsername(), user.getEmail());
+    }
+}
