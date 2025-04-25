476fd3582d0ab22affc11f18138356be5a149e84
Samuel <samuelmorais552@gmail.com>
2025-04-25 15:52:55 -0300

    chore: Adicao de dependencia para exportar variavieis de ambiente, caso ja nao tenha sido feito

--- backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/EchoTyperApplication.java
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/EchoTyperApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EchoTyperApplication {
 
 	public static void main(String[] args) {
+		io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(EchoTyperApplication.class, args);
 	}
 
