4dc205e428b7d881e75107171271b578ff5c08ba
Kael Paraguassu <kaelparaguassu@hotmail.com>
2025-04-25 13:35:23 -0300

    criando service do Meeting

---
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/meeting/service/MeetingConstraints.java
@@ -0,0 +1,7 @@
+package br.ufrn.EchoTyper.meeting.service;
+
+
+public class MeetingConstraints {
+   public static final String TITLE_PATTERN = "^[^*/$#%&-]*$";
+   public static final String DESCRIPTION_PATTERN = "^[^*/$#%&-]*$";
+}
