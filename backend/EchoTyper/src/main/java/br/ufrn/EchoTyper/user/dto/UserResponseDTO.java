a258ab82167ae77d006b3d7306afbf2d243f0063
Kael Paraguassu <kaelparaguassu@hotmail.com>
2025-05-11 01:06:52 -0300

    adicionando id na estrutura do responseDTO

--- backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/user/dto/UserResponseDTO.java
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/user/dto/UserResponseDTO.java
@@ -3,6 +3,7 @@ package br.ufrn.EchoTyper.user.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record UserResponseDTO(
+        @JsonProperty("id") Long id,
         @JsonProperty("username") String username,
         @JsonProperty("email") String email) {
 }
