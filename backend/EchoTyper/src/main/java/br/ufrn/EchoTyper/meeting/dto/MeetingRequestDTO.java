f4f27b9940421de1dd8d6302ab67c051bd494a2e
Samuel <samuelmorais552@gmail.com>
2025-05-25 11:41:24 -0300

    Refactoring the Meeting's DTO layer to account for the MeetingGroup relation

--- backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/meeting/dto/MeetingRequestDTO.java
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/meeting/dto/MeetingRequestDTO.java
@@ -1,6 +1,5 @@
 package br.ufrn.EchoTyper.meeting.dto;
 
-
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,22 +9,14 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record MeetingRequestDTO(
-        @NotNull
-        @JsonProperty("title")
-        @Length(min = 6, max = 50)
-        @Pattern(regexp = MeetingConstraints.TITLE_PATTERN) 
-        String title,
-        @JsonProperty("transcription") 
-        @NotNull
-        String transcription,
-        @JsonProperty("summary")
-        @NotNull
-        String summary,
-        @JsonProperty("annotations")
-        @NotNull
-        String annotations
-        )
+                @NotNull @JsonProperty("title") @Length(min = 6, max = 50) @Pattern(regexp = MeetingConstraints.TITLE_PATTERN) String title,
+                @JsonProperty("transcription") @NotNull String transcription,
+                @JsonProperty("summary") @NotNull String summary,
+                @JsonProperty("annotations") @NotNull String annotations,
+                @JsonProperty("groupId") Long groupId)
 
 {
-
+        public MeetingRequestDTO(String title, String transcription, String summary, String annotations) {
+                this(title, transcription, summary, annotations, null);
+        }
 }
