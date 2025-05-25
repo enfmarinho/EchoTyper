c8cb546cb4ca2e370a928181ece6dda2317ac191
Samuel <samuelmorais552@gmail.com>
2025-05-25 11:40:25 -0300

    Added implementations for the MeetingGroup controller methods

--- backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/meeting/controller/MeetingControllerImpl.java
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/meeting/controller/MeetingControllerImpl.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufrn.EchoTyper.meeting.service.MeetingService;
+import br.ufrn.EchoTyper.meetingGroup.dto.MeetingGroupRequestDTO;
+import br.ufrn.EchoTyper.meetingGroup.dto.MeetingGroupResponseDTO;
+import jakarta.validation.Valid;
 import br.ufrn.EchoTyper.meeting.dto.MeetingResponseDTO;
 import br.ufrn.EchoTyper.meeting.dto.MeetingRequestDTO;
 
@@ -39,5 +42,35 @@ public class MeetingControllerImpl implements MeetingController {
     public ResponseEntity<MeetingResponseDTO> deleteMeeting(Long id) {
         meetingService.deleteMeeting(id);
         return ResponseEntity.ok().build();
+    }
+    @Override
+    public ResponseEntity<List<MeetingResponseDTO>> getGrouplessMeetings() {
+        return ResponseEntity.ok().body(meetingService.getGrouplessMeetings());
+    }
+
+    @Override
+    public ResponseEntity<List<MeetingGroupResponseDTO>> getAllGroups() {
+        return ResponseEntity.ok().body(meetingService.getAllGroups());
+    }
+
+    @Override
+    public ResponseEntity deleteGroup(Long groupId) {
+        meetingService.deleteMeetingGroup(groupId);
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public ResponseEntity<MeetingGroupResponseDTO> addMeeting(Long meetingId, Long groupId) {
+        return ResponseEntity.ok().body(meetingService.addMeetingToGroup(meetingId, groupId));
+    }
+
+    @Override
+    public ResponseEntity<MeetingGroupResponseDTO> removeMeeting(Long meetingId, Long groupId) {
+        return ResponseEntity.ok().body(meetingService.removeMeetingFromGroup(meetingId, groupId));
+    }
+
+    @Override
+    public ResponseEntity<MeetingGroupResponseDTO> createGroup(@Valid MeetingGroupRequestDTO createGroupDTO) {
+        return ResponseEntity.ok().body(meetingService.createGroup(createGroupDTO));
     }
 }
