499e68cfc2b9c2650047c09f2939c9e165d84cf0
Samuel <samuelmorais552@gmail.com>
2025-05-25 11:39:47 -0300

    Added MeetingGroup endpoints to the Meeting controller. These resources have to share the same controller to avoid a circular dependency error with the MeetingService and MeetinbgGroupservice classes

--- backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/meeting/controller/MeetingController.java
+++ backend/EchoTyper/src/main/java/br/ufrn/EchoTyper/meeting/controller/MeetingController.java
@@ -7,13 +7,18 @@ import org.springframework.http.ResponseEntity;
 
 import br.ufrn.EchoTyper.meeting.dto.MeetingRequestDTO;
 import br.ufrn.EchoTyper.meeting.dto.MeetingResponseDTO;
+import br.ufrn.EchoTyper.meetingGroup.dto.MeetingGroupRequestDTO;
+import br.ufrn.EchoTyper.meetingGroup.dto.MeetingGroupResponseDTO;
 import jakarta.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/meetings")
 public interface MeetingController {
-    @GetMapping
+    @GetMapping("")
     public ResponseEntity<List<MeetingResponseDTO>> getAllMeetings();
+
+    @GetMapping("/groupless")
+    public ResponseEntity<List<MeetingResponseDTO>> getGrouplessMeetings();
 
     @GetMapping("/{id}")
     public ResponseEntity<MeetingResponseDTO> getMeetingById(@PathVariable("id") Long id);
@@ -26,4 +31,21 @@ public interface MeetingController {
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<MeetingResponseDTO> deleteMeeting(@PathVariable("id") Long id);
+
+    @GetMapping("/groups")
+    public ResponseEntity<List<MeetingGroupResponseDTO>> getAllGroups();
+
+    @PostMapping("/groups/create")
+    public ResponseEntity<MeetingGroupResponseDTO> createGroup(@Valid @RequestBody MeetingGroupRequestDTO createGroupDTO);
+
+    @PutMapping("/groups/add/{meetingId}/{groupId}")
+    public ResponseEntity<MeetingGroupResponseDTO> addMeeting(@PathVariable("meetingId") Long meetingId,
+            @PathVariable("groupId") Long groupId);
+
+    @PutMapping("/groups/remove/{meetingId}/{groupId}")
+    public ResponseEntity<MeetingGroupResponseDTO> removeMeeting(@PathVariable("meetingId") Long meetingId,
+            @PathVariable("groupId") Long groupId);
+
+    @DeleteMapping("/groups/delete/{groupId}")
+    public ResponseEntity deleteGroup(@PathVariable("groupId") Long groupId);
 }
