fb55033772dad70a2c24f7f84796278174a51854
Kael Paraguassu <kaelparaguassu@hotmail.com>
2025-06-28 16:59:45 -0300

    adaptando GeminiApiService com as strategies

--- src/backend/src/main/java/br/ufrn/EchoTyper/LLM/service/GeminiApiService.java
+++ src/backend/src/main/java/br/ufrn/EchoTyper/LLM/service/GeminiApiService.java
@@ -2,9 +2,11 @@ package br.ufrn.EchoTyper.LLM.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.json.Json;
 
-import br.ufrn.EchoTyper.meeting.service.MeetingService;
+import br.ufrn.EchoTyper.LLM.service.CheckConflictStrategy.CheckConflictDefaultStrategy;
+import br.ufrn.EchoTyper.LLM.service.CheckConflictStrategy.CheckConflictStrategy;
+import br.ufrn.EchoTyper.LLM.service.SummaryStrategy.SummaryDefaultStrategy;
+import br.ufrn.EchoTyper.LLM.service.SummaryStrategy.SummaryStrategy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,20 +21,40 @@ public class GeminiApiService implements LLM_Interface {
   private String apiKey;
 
   @Autowired
-  private MeetingService meetingService;
+  private SummaryStrategy summaryStrategy = new SummaryDefaultStrategy();
+
+  @Autowired
+  private CheckConflictStrategy checkConflictStrategy = new CheckConflictDefaultStrategy();
 
   private final RestTemplate restTemplate = new RestTemplate();
   private final ObjectMapper objectMapper = new ObjectMapper();
   private String baseUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=";
 
   public JsonNode checkConflicts(JsonNode payload) throws Exception {
-    String prompt = buildConflictPrompt(payload);
-    return callGeminiApi(prompt);
+    if (payload == null || payload.isEmpty()) {
+      throw new IllegalArgumentException("Payload cannot be null or empty");
+    }
+
+    try {
+      String prompt = checkConflictStrategy.buildConflictPrompt(payload);
+      return callGeminiApi(prompt);
+    } catch (Exception e) {
+      throw new Exception("Error when trying to check conflicts: " + e.getMessage(), e);
+    }
   }
 
   public JsonNode summarize(JsonNode payload) throws Exception {
-    String prompt = buildSummaryPrompt(payload);
-    return callGeminiApi(prompt);
+    if (payload == null || payload.isEmpty()) {
+      throw new IllegalArgumentException("Payload cannot be null or empty");
+    }
+
+    try {
+      String prompt = summaryStrategy.buildSummaryPrompt(payload);
+      return callGeminiApi(prompt);
+
+    } catch (Exception e) {
+      throw new Exception("Error when trying to summarize: " + e.getMessage(), e);
+    }
   }
 
   private JsonNode callGeminiApi(String prompt) throws Exception {
@@ -59,59 +81,7 @@ public class GeminiApiService implements LLM_Interface {
     return objectMapper.readTree(response.getBody());
   }
 
-  private String buildConflictPrompt(JsonNode payload) {
-    String transcription = payload.path("transcription").asText();
-    String events = payload.path("events").toString();
-
-    return """
-        A seguir está a transcrição de uma reunião:
-        %s
-
-        E abaixo está a lista de eventos já agendados (JSON):
-        %s
-
-        Verifique se há conflitos de data entre o que foi dito na reunião e os eventos agendados.
-        Se houver conflitos, retorne os IDs dos eventos em conflito e com qual evento na transcrição eles conflitam.
-        Se não houver conflitos, diga que não há conflitos.
-        """
-        .formatted(transcription, events);
-  }
-
-  private String buildSummaryPrompt(JsonNode payload) {
-    String transcription = payload.path("transcription").asText();
-    JsonNode groupIdNode = payload.path("groupId");
-    if (groupIdNode.isMissingNode()) {
-
-      return """
-          A seguir está a transcrição de uma reunião:
-          %s
-
-          Crie um breve resumo da reunião, organizando em tópicos e detalhando apenas os pontos mais relevantes.
-          """
-          .formatted(transcription);
-    }
-    return """
-        A seguir está a transcrição de uma reunião:
-        %s
-
-        Crie um breve resumo da reunião, organizando em tópicos e detalhando apenas os pontos mais relevantes.
-        A seguir, ha uma lista com resumos de reunioes relacionadas a atual. Caso esta lista nao esteja vazia, use o contexto destes resumos para complementar
-        o resumo gerado para a reuniao atual, quando possivel ou adequado.
-        %s
-        """
-        .formatted(transcription, getSummaryContext(groupIdNode.asLong()));
-
-  }
-
   private String escapeJson(String input) {
     return input.replace("\"", "\\\"").replace("\n", "\\n");
-  }
-
-  private String getSummaryContext(Long id) {
-    StringBuilder builder = new StringBuilder("[");
-    meetingService.getSummariesByGroup(id).stream()
-        .forEach((summary) -> builder.append(String.format("\"%s\"%n", summary)));
-    builder.append("]");
-    return builder.toString();
   }
 }
