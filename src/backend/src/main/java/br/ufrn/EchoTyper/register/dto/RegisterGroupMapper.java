4924071bd7994db0bdec54f27667da705f05fdce
Samuel <samuelmorais552@gmail.com>
2025-07-06 17:50:06 -0300

    Replacing reflection with the RegisterFactory class on RegisterMapper and RegisterGroupMapper

---
+++ src/backend/src/main/java/br/ufrn/EchoTyper/register/dto/RegisterGroupMapper.java
@@ -0,0 +1,44 @@
+package br.ufrn.EchoTyper.register.dto;
+
+import java.lang.reflect.Constructor;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.ufrn.EchoTyper.register.dto.factories.RegisterGroupFactory;
+import br.ufrn.EchoTyper.register.model.Register;
+import br.ufrn.EchoTyper.register.model.RegisterGroup;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@Component
+public class RegisterGroupMapper<RegisterGroupImpl extends RegisterGroup, RegisterImpl extends Register> {
+
+    private RegisterMapper<RegisterImpl> registerMapper;
+
+    private final RegisterGroupFactory<RegisterGroupImpl, RegisterImpl> registerGroupFactory;
+
+    @Autowired
+    public RegisterGroupMapper(RegisterGroupFactory<RegisterGroupImpl, RegisterImpl> registerGroupFactory,
+            RegisterMapper<RegisterImpl> registerMapper) {
+        this.registerGroupFactory = registerGroupFactory;
+        this.registerMapper = registerMapper;
+    }
+
+    public RegisterGroupImpl toEntity(RegisterGroupRequestDTO registerGroupRequestDTO, Set<RegisterImpl> registers) {
+        return registerGroupFactory.createGroup(null, registerGroupRequestDTO.groupName(), registers,
+                registerGroupRequestDTO.content());
+    }
+
+    public RegisterGroupResponseDTO toResponseDTO(RegisterGroupImpl registerGroup) {
+        Collection<RegisterResponseDTO> registerDTOs = registerMapper.toResponseDTO(registerGroup.getRegisters());
+        Set<RegisterResponseDTO> registerDTOsSet = new HashSet<>(registerDTOs);
+        return new RegisterGroupResponseDTO(registerGroup.getId(), registerGroup.getGroupName(), registerDTOsSet,
+                registerGroup.getContent());
+    }
+}
--- src/backend/src/main/java/br/ufrn/EchoTyper/register/dto/RegisterMapper.java
+++ src/backend/src/main/java/br/ufrn/EchoTyper/register/dto/RegisterMapper.java
@@ -1,62 +1,36 @@
 package br.ufrn.EchoTyper.register.dto;
 
 import java.util.Collection;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.lang.Class;
-import java.lang.reflect.Constructor;
-
+import br.ufrn.EchoTyper.register.dto.factories.RegisterFactory;
 import br.ufrn.EchoTyper.register.model.Register;
-import br.ufrn.EchoTyper.registerGroup.model.RegisterGroup;
-import com.fasterxml.jackson.databind.JsonNode;
-import java.util.logging.Logger;
+import br.ufrn.EchoTyper.register.model.RegisterGroup;
+
 
 @Component
-public class RegisterMapper {
-    private Constructor<? extends Register> fullConstructor;
-    private Constructor<? extends Register> grouplessConstructor;
-    private static final Logger LOG = Logger.getLogger(RegisterMapper.class.getName());
+public class RegisterMapper<RegisterImpl extends Register> {
+
+    private final RegisterFactory<RegisterImpl> registerFactory;
 
     @Autowired
-    public RegisterMapper(Register register) {
-        // ! Remover o exception swallowing
-        try {
-            Class<? extends Register> registerClass = register.getClass();
-            this.grouplessConstructor = registerClass.getConstructor(Long.class, String.class, String.class,
-                    String.class, String.class, JsonNode.class);
-            this.fullConstructor = registerClass.getConstructor(Long.class, String.class, String.class,
-                    String.class, String.class, RegisterGroup.class,JsonNode.class);
-        } catch (Exception e) {
-            LOG.severe("An exception has occurred" + e.getMessage());
-        }
+    public RegisterMapper(RegisterFactory<RegisterImpl> registerFactory) {
+        this.registerFactory = registerFactory;
     }
 
-    public Register toEntity(RegisterRequestDTO registerRequestDTO) {
-        Register register = null;
-        try {
-            register = grouplessConstructor.newInstance(null, registerRequestDTO.title(),
-                    registerRequestDTO.transcription(),
-                    registerRequestDTO.summary(), registerRequestDTO.annotations(),
-                    registerRequestDTO.content());
-        } catch (Exception e) {
-            LOG.severe("An exception has occurred" + e.getMessage());
-        }
-        return register;
+    public RegisterImpl toEntity(RegisterRequestDTO registerRequestDTO) {
+        return registerFactory.createSoloRegister(null, registerRequestDTO.title(), registerRequestDTO.transcription(),
+                registerRequestDTO.summary(), registerRequestDTO.annotations(),
+                registerRequestDTO.content());
     }
 
-    public Register toEntity(RegisterRequestDTO registerRequestDTO, RegisterGroup group) {
-        Register register = null;
-        try {
-            register = fullConstructor.newInstance(null, registerRequestDTO.title(), registerRequestDTO.transcription(),
-                    registerRequestDTO.summary(), registerRequestDTO.annotations(), group,
-                    registerRequestDTO.content());
-        } catch (Exception e) {
-            LOG.severe("An exception has occurred" + e.getMessage());
-        }
-        return register;
+    public RegisterImpl toEntity(RegisterRequestDTO registerRequestDTO, RegisterGroup group) {
+        return registerFactory.createGroupedRegister(null, registerRequestDTO.title(),
+                registerRequestDTO.transcription(),
+                registerRequestDTO.summary(), registerRequestDTO.annotations(), group,
+                registerRequestDTO.content());
     }
 
     public RegisterResponseDTO toResponseDTO(Register register) {
