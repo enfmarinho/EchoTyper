bcab883bc67af1d25b0e0843ae149170a161b71c
Samuel <samuelmorais552@gmail.com>
2025-07-06 18:41:05 -0300

    Proper generics usage on Meeting

--- src/backend/src/main/java/br/ufrn/EchoTyper/meeting/model/Meeting.java
+++ src/backend/src/main/java/br/ufrn/EchoTyper/meeting/model/Meeting.java
@@ -84,7 +84,7 @@ public class Meeting extends Register {
 
     // ! : O uso do RegisterGroup como parametro pode causar erro
     public Meeting(Long id, String title, String transcription, String summary, String annotations,
-            RegisterGroup group, JsonNode content) {
+            RegisterGroup<Meeting> group, JsonNode content) {
         super(id, title, transcription, summary, annotations,
                 group, content);
     }
