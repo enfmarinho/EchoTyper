146f129a6ae2dcd1e3e0f98a580f42aff9760bc2
Samuel <samuelmorais552@gmail.com>
2025-07-06 18:11:51 -0300

    New Meeting and MeetingGroup implementations, providing implementations to the new abstract methods

--- src/backend/src/main/java/br/ufrn/EchoTyper/meeting/model/Meeting.java
+++ src/backend/src/main/java/br/ufrn/EchoTyper/meeting/model/Meeting.java
@@ -1,20 +1,21 @@
 package br.ufrn.EchoTyper.meeting.model;
 
 import br.ufrn.EchoTyper.register.model.Register;
-import br.ufrn.EchoTyper.registerGroup.model.RegisterGroup;
+import br.ufrn.EchoTyper.register.model.RegisterGroup;
+import br.ufrn.EchoTyper.utils.JsonUtil;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import java.security.Timestamp;
 import java.time.Instant;
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.springframework.stereotype.Component;
@@ -24,18 +25,51 @@ import org.springframework.stereotype.Component;
 @Table(name = "tb_meeting")
 public class Meeting extends Register {
 
+    @Column(name = "str_participants")
+    Set<String> participants;
 
-    @Column(nullable = false, name = "str_participants")
-    Set<String> participants ;
+    @Column(nullable = false, name = "dt_date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date date = Date.from(Instant.now());
 
-    public void setParticipants(Set<String> participants) {
+    protected void setParticipants(Set<String> participants) {
         this.participants = participants;
     }
 
-    // Nao sei se a deserializacao da string nos respectivos objetos deve ser feita
-    // na subclasse ou na classe abstrata 
-    public void computeParticipants(Set<String> participants) {
-        this.participants = participants;
+    protected Set<String> getParticipants() {
+        return this.participants;
+    }
+
+    protected void setDate(Date date) {
+        this.date = date;
+    }
+
+    protected Date getDate() {
+        return this.date;
+    }
+
+    @Override
+    public void setContent(JsonNode json) {
+        Set<String> participants = JsonUtil.deserialize(json.get("participants"),
+                new TypeReference<Set<String>>() {
+                });
+        this.setParticipants(participants);
+
+        if (json.has("date")) {
+            Date date = JsonUtil.deserialize(json.get("date"),
+                    new TypeReference<Date>() {
+                    });
+            this.setDate(date);
+        }
+    }
+
+    @Override
+    public JsonNode getContent() {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode contentNode = mapper.createObjectNode();
+        contentNode.put("date", this.getDate().toString());
+        contentNode.putPOJO("participants", this.getParticipants());
+        return contentNode;
     }
 
     public Meeting() {
@@ -45,13 +79,13 @@ public class Meeting extends Register {
     public Meeting(Long id, String title, String transcription, String summary, String annotations,
             JsonNode content) {
         super(id, title, transcription, summary, annotations,
-            content);
+                content);
     }
 
     // ! : O uso do RegisterGroup como parametro pode causar erro
     public Meeting(Long id, String title, String transcription, String summary, String annotations,
             RegisterGroup group, JsonNode content) {
-        super(id,title,transcription, summary,annotations,
-            group, content);
+        super(id, title, transcription, summary, annotations,
+                group, content);
     }
 }
--- src/backend/src/main/java/br/ufrn/EchoTyper/meeting/model/MeetingGroup.java
+++ src/backend/src/main/java/br/ufrn/EchoTyper/meeting/model/MeetingGroup.java
@@ -1,11 +1,17 @@
 package br.ufrn.EchoTyper.meeting.model;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
-import br.ufrn.EchoTyper.registerGroup.model.RegisterGroup;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import br.ufrn.EchoTyper.register.model.Register;
+import br.ufrn.EchoTyper.register.model.RegisterGroup;
+import br.ufrn.EchoTyper.utils.JsonUtil;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -14,11 +20,42 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_meeting_group")
 public class MeetingGroup extends RegisterGroup {
-    @Column(nullable = false, name = "str_participants")
-    List<String> participants;
+    @Column(name = "str_participants")
+    Set<String> participants;
 
-    public void computeParticipants(List<String> participants) {
+    public void setParticipants(Set<String> participants) {
         this.participants = participants;
     }
 
+    public Set<String> getParticipants() {
+        return participants;
+    }
+
+    @Override
+    public void setContent(JsonNode json) {
+        Set<String> participants = JsonUtil.deserialize(json,
+                new TypeReference<Set<String>>() {
+                });
+        this.setParticipants(participants);
+    }
+
+    @Override
+    public JsonNode getContent() {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode contentNode = mapper.createObjectNode();
+        contentNode.putPOJO("participants", this.getParticipants());
+        ObjectNode finalJson = mapper.createObjectNode();
+        finalJson.set("content", contentNode);
+        return finalJson;
+    }
+
+    public MeetingGroup() {
+    }
+
+    public MeetingGroup(Long id, String groupName, Set<Register> registers, JsonNode content) {
+        this.groupName = groupName;
+        this.registers = registers;
+        this.id = id;
+        setContent(content);
+    }
 }
