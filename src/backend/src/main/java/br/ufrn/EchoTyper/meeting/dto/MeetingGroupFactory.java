395a26a9085fc275fb95a1eaf05926928ab0509c
Samuel <samuelmorais552@gmail.com>
2025-07-06 18:40:18 -0300

    Fixing the generics usage on  MeetingGroupFactory

--- src/backend/src/main/java/br/ufrn/EchoTyper/meeting/dto/MeetingGroupFactory.java
+++ src/backend/src/main/java/br/ufrn/EchoTyper/meeting/dto/MeetingGroupFactory.java
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import br.ufrn.EchoTyper.meeting.model.Meeting;
 import br.ufrn.EchoTyper.meeting.model.MeetingGroup;
 import br.ufrn.EchoTyper.register.dto.factories.RegisterGroupFactory;
 import br.ufrn.EchoTyper.register.model.Register;
 
 @Component
-public class MeetingGroupFactory implements RegisterGroupFactory<MeetingGroup> 
+public class MeetingGroupFactory implements RegisterGroupFactory<MeetingGroup, Meeting> 
 {
 
     @Override
-    public MeetingGroup createGroup(Long id, String groupName, Set<Register> registers, JsonNode content) {
+    public MeetingGroup createGroup(Long id, String groupName, Set<Meeting> registers, JsonNode content) {
         return new MeetingGroup(id, groupName, registers, content);
     }
     
