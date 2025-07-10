b4e9c0ce30393f6df7e5f317f7d0ae88fbdb5ab7
Kael Paraguassu <kaelparaguassu@hotmail.com>
2025-07-10 17:38:17 -0300

    Implementando subclasse concreta SummaryInterview

--- src/backend/src/main/java/br/ufrn/EchoTyper/LLM/service/SummaryStrategies/SummaryStrategy.java
+++
@@ -1,8 +0,0 @@
-package br.ufrn.EchoTyper.LLM.service.SummaryStrategies;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface SummaryStrategy {
-    String buildSummaryPrompt(JsonNode payload);
-    String getContext(long groupId);
-}
