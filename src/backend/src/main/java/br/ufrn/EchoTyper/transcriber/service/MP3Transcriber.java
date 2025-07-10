faca48f0fea196e710bf753c588a7c5871dc0bcb
enfmarinho <eduardonestormarinho228@gmail.com>
2025-07-10 20:52:53 -0300

    Adding missing includes

--- src/backend/src/main/java/br/ufrn/EchoTyper/transcriber/service/MP3Transcriber.java
+++ src/backend/src/main/java/br/ufrn/EchoTyper/transcriber/service/MP3Transcriber.java
@@ -1,5 +1,8 @@
 package br.ufrn.EchoTyper.transcriber.service;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 @Service
 @Qualifier("mp3Transcriber")
 public class MP3Transcriber extends TranscriberTemplate {
